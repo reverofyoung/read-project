5a2b3b6cbf4c091926e3fc1585315c3df70cc2f9
260 <reverofyoung@gmail.com>
2023-05-24 13:37:33 +0900

    복구

--- pages/Home.js
+++ pages/Home.js
@@ -2,37 +2,26 @@ import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-import CreateReport from "./CreateReport";
-
 function Home({navigation}) {
   const [menuVisible, setMenuVisible] = useState(false);
 
-  const menuOpen = () => {
-    console.log('클릭');
-  };
-  
   const onClickMenuBtn = () => {
     !menuVisible ? setMenuVisible(true) : setMenuVisible(false);
   };
 
   return (
     <View style={styles.container}>
-      {/* <TouchableOpacity onPress={ () => navigation.openDrawer() }>
-        <Text>메뉴</Text>
-      </TouchableOpacity> */}
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <View style={styles.container}>
-          <Button title="글쓰기로 가용" onPress={() => navigation.navigate('CreateReport')} />
-        </View>
+      <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+
         {/* 메뉴 버튼 0n/Off */}
-        {/* <TouchableOpacity onPress={ onClickMenuBtn }>
+        <TouchableOpacity onPress={ onClickMenuBtn }>
           { 
             !menuVisible ? <Text style={{ padding: 5, borderRadius: 20 }}>⭕️</Text> : 
             <Text style={{ padding: 5, borderRadius: 20 }}>❌</Text> 
           }
-        </TouchableOpacity> */}
+        </TouchableOpacity>
 
-        {/* <View style={{ flexDirection: 'row', display: !menuVisible ? 'none' : 'flex'}}>
+        <View style={{ flexDirection: 'row', display: !menuVisible ? 'none' : 'flex'}}>
           <Button 
             title={ '책검색' } 
             onPress={ () => navigation.navigate('Search') }
@@ -45,7 +34,7 @@ function Home({navigation}) {
             title={ '글쓰기' }
             onPress={ () => navigation.navigate('CreateReport') }
           />
-        </View> */}
+        </View>
 
       </View>
     </View>
